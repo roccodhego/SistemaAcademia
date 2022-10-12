955a905221e69dfb99572da764251300f72e4afa
Moura <mouradiego454@gmail.com>
2022-10-12 14:37:30 -0300

    Path do bd relativo, funções dml e dql
    Add caminho relativo para o banco de dados, ou seja, configurar para que o sistema encontre o banco de dados mesmo que seja movido para outra pasta. 
    Criada novas funções para manipulação do banco de dados, dml (data manipulation language) para insert, delete e update e também dql (data query language) para select.

--- SistemaAcademia/Banco.cs
+++ SistemaAcademia/Banco.cs
@@ -13,12 +13,70 @@ namespace SistemaAcademia
     {
         private static SQLiteConnection conexao;
 
+        //Funcções Genéricas
+
         private static SQLiteConnection ConexaoBanco()
         {
-            conexao = new SQLiteConnection("Data Source = G:\\Curso\\ProjetoIntegrador\\SistemaAcademia\\banco\\banco_academia.db");
+            conexao = new SQLiteConnection("Data Source = "+ Globais.caminhoBanco + Globais.nomeBanco);
             conexao.Open();
             return conexao;
         }
+
+
+        public static DataTable dql(string sql) // Data Query Language (Select) = Consulta
+        {
+            SQLiteDataAdapter da = null;
+            DataTable dt = new DataTable();
+            try
+            {
+                var vcon = ConexaoBanco();
+                var cmd = vcon.CreateCommand();
+                cmd.CommandText = sql;
+                da = new SQLiteDataAdapter(cmd.CommandText, vcon);
+                da.Fill(dt);
+                vcon.Close();
+                return dt;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+
+            }
+        }
+
+
+        public static DataTable dml(string q, string msgOK=null, string msgERRO=null) //Data manipulation Language (Insert, Delete, Update)
+        {
+            SQLiteDataAdapter da = null;
+            DataTable dt = new DataTable();
+            try
+            {
+                var vcon = ConexaoBanco();      //Abrindo uma conexão para o metodo
+                var cmd = vcon.CreateCommand();
+                cmd.CommandText = q;
+                da = new SQLiteDataAdapter(cmd.CommandText, vcon);
+                cmd.ExecuteNonQuery();
+                vcon.Close(); // fechando essa conexão
+
+                if (msgOK != null)
+                { 
+                    MessageBox.Show(msgOK);
+                }
+            }
+            catch (Exception ex)
+            {
+                if (msgERRO != null)
+                {
+                    MessageBox.Show(msgERRO + "\n" + ex.Message);
+                }
+                throw ex;
+
+            }
+        }
+
+
+
+
 
         public static DataTable ObterTodosUsuarios() // Retornar um DataTable com todos os usuários
         {
@@ -41,26 +99,6 @@ namespace SistemaAcademia
             }
         }
 
-        public static DataTable consulta(string sql)
-        {
-            SQLiteDataAdapter da = null;
-            DataTable dt = new DataTable();
-            try
-            {
-                var vcon = ConexaoBanco();
-                var cmd = vcon.CreateCommand();                
-                cmd.CommandText = sql;
-                da = new SQLiteDataAdapter(cmd.CommandText, vcon);
-                da.Fill(dt);
-                vcon.Close();
-                return dt;  
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-
-            }
-        }
 
         // Funções do FORM F_GestaoUsuarios
         public static DataTable ObterUsuariosIdNome() // Retornar um DataTable com todos os usuários
